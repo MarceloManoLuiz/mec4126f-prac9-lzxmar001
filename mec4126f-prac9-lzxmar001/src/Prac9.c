272023def7e3825624b3dff7888c6a6e27caa124
Marcelo Luiz <LZXMAR001@myuct.ac.za>
2021-06-11 14:35:59 +0200

    Commit entire Prac 9 initially

---
+++ mec4126f-prac9-lzxmar001/src/Prac9.c
@@ -0,0 +1,121 @@
+// Description----------------------------------------------------------------|
+/*
+ * Initialises a struct with Name and Age data. Displays results on LEDs and
+ * LCD.
+ */
+// DEFINES AND INCLUDES-------------------------------------------------------|
+
+#define STM32F051
+//>>> Uncomment line 10 if using System Workbench (SW4STM32) or STM32CubeIDE
+#define SW4STM32
+
+#ifndef SW4STM32
+	#define TRUESTUDIO
+#endif
+
+#include "stm32f0xx.h"
+
+// GLOBAL VARIABLES ----------------------------------------------------------|
+
+
+// FUNCTION DECLARATIONS -----------------------------------------------------|
+
+void main(void);                                                   //COMPULSORY
+void init_ADC(void);											   //COMPULSORY
+void init_timer_2(void);
+#ifdef TRUESTUDIO												   //COMPULSORY
+	void reset_clock_to_48Mhz(void);							   //COMPULSORY
+#endif															   //COMPULSORY
+
+
+
+// MAIN FUNCTION -------------------------------------------------------------|
+
+void main(void)
+{
+#ifdef TRUESTUDIO  											 	   //COMPULSORY
+	reset_clock_to_48Mhz();										   //COMPULSORY
+#endif															   //COMPULSORY
+
+	init_timer_2();
+	init_timer_6();
+
+	while(1)
+	{
+
+	}
+}
+
+// OTHER FUNCTIONS -----------------------------------------------------------|
+
+#ifdef TRUESTUDIO												   //COMPULSORY
+/* Description:
+ * This function resets the STM32 Clocks to 48 MHz
+ */
+void reset_clock_to_48Mhz(void)									   //COMPULSORY
+{																   //COMPULSORY
+	if ((RCC->CFGR & RCC_CFGR_SWS) == RCC_CFGR_SWS_PLL)			   //COMPULSORY
+	{															   //COMPULSORY
+		RCC->CFGR &= (uint32_t) (~RCC_CFGR_SW);					   //COMPULSORY
+		while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI);	   //COMPULSORY
+	}															   //COMPULSORY
+
+	RCC->CR &= (uint32_t)(~RCC_CR_PLLON);						   //COMPULSORY
+	while ((RCC->CR & RCC_CR_PLLRDY) != 0);						   //COMPULSORY
+	RCC->CFGR = ((RCC->CFGR & (~0x003C0000)) | 0x00280000);		   //COMPULSORY
+	RCC->CR |= RCC_CR_PLLON;									   //COMPULSORY
+	while ((RCC->CR & RCC_CR_PLLRDY) == 0);						   //COMPULSORY
+	RCC->CFGR |= (uint32_t) (RCC_CFGR_SW_PLL);					   //COMPULSORY
+	while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);		   //COMPULSORY
+}																   //COMPULSORY
+#endif															   //COMPULSORY
+
+void init_ADC(void)
+{
+	//
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	//
+	GPIOA->MODER |= GPIO_MODER_MODER6;
+	//
+	RCC->APB2ENR |= RCC_APB2ENR_ADCEN;
+	//
+	ADC1->CR |= ADC_CR_ADEN;
+	//
+	ADC1->CHSELR |= ADC_CHSELR_CHSEL6;
+	// Setup in wait mode
+	ADC1->CFGR1 |= ADC_CFGR1_WAIT;
+	// Setup 10 bit resolution
+
+	//
+	while((ADC1->ISR & ADC_ISR_ADRDY) == 0);
+}
+
+void init_timer_2(void)		
+{
+	RCC -> APB1ENR |= RCC_APB1ENR_TIM2EN;	// Set clock to timer 2
+	RCC -> AHBENR |= RCC_AHBENR_GPIOBEN;	// Enable clock for Port B
+	GPIOB -> MODER |= GPIO_MODER_MODER10_1;	// Set B10 to alternate function
+	GPIOB -> AFR[1] |= 0x02 << (4*2);		// Set AFR to AF2 for B10 (TIM2_CH3)
+
+	TIM2 -> PSC = 0;						// Set PSC to 0 such that counter counts at the rate of f_CK_PSC
+	TIM2 -> ARR = 3200;						// Set ARR to 3200 to achieve PWM signal of 15kHz
+	TIM2 -> CCMR1 |= TIM_CCMR2_OC3M_2
+				  | TIM_CCMR2_OC3M_1 	    // Configure PWM mode on OC3 (OC3 because AF2 for B10 is TIM2_CH3!)
+				  | TIM_CCMR2_OC3PE; 	    // Enable preload register on OC3
+	TIM2 -> CCER |= TIM_CCER_CC3E;			// Enable output on OC3
+	TIM2 -> CR1 |= TIM_CR1_CEN;				// Enable counter
+	
+	TIM2 -> CCR3 = 3200/4;					// Configure duty cycle for 25% of ARR (using CCR3 because TIM2_CH3)
+}
+
+void init_timer_6(void)
+{					
+	TIM6_DAC_IRQn
+}
+
+// INTERRUPT HANDLERS --------------------------------------------------------|
+
+void TIM6_DAC_IRQHandler(void)
+{				
+	TIM6->SR &= ~TIM_SR_UIF;	//acknowledge interrupt
+}
